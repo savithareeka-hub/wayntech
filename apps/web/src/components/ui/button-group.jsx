259f1f3fbe18235507aa3ee69c7973a36679c282
savithareeka-hub <savithareeka@gmail.com>
2026-03-29 23:05:22 +0530

    Update button-group.jsx

--- apps/web/src/components/ui/button-group.jsx
+++ apps/web/src/components/ui/button-group.jsx
@@ -1,7 +1,7 @@
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 import { Separator } from "@/components/ui/separator"
 
 const buttonGroupVariants = cva(
